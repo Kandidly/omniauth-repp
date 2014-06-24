d1050a9b6d04fce762058846936e988a368e53ed
Alex Sears <alexwsears@gmail.com>
2014-06-24 11:35:27 -0400

    Updated: Info grabbed and send basic auth headers to authorize the request

--- lib/omniauth/strategies/repp.rb
+++ lib/omniauth/strategies/repp.rb
@@ -3,12 +3,17 @@ require 'multi_json'
 
 module OmniAuth
   module Strategies
-    class Repp < OmniAuth::Strategies::OAuth
+    class Repp < OmniAuth::Strategies::OAuth2
+      class NoAuthorizationCodeError < StandardError; end
+      class UnknownSignatureAlgorithmError < NotImplementedError; end
+
       option :name, 'repp'
 
-      option :client_options, {:authorize_path => '/oauth/authorize',
-                               :site => 'https://api.myrepp.com',
-                               :proxy => ENV['http_proxy'] ? URI(ENV['http_proxy']) : nil}
+      option :client_options, {
+        :site => 'https://api.myrepp.com',
+        :authorize_url => '/oauth/authorize',
+        :token_url => '/oauth/token'
+      }
 
       uid { access_token.params[:user_id] }
 
@@ -31,9 +36,34 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= MultiJson.load(access_token.get('/people/me').body)
-      rescue ::Errno::ETIMEDOUT
-        raise ::Timeout::Error
+        @raw_info ||= access_token.get('/people/me').parsed || {}
+      end
+
+      def callback_phase
+        if request.params.key?('code')
+          super
+        end
+      rescue NoAuthorizationCodeError => e
+        fail!(:no_authorization_code, e)
+      rescue UnknownSignatureAlgorithmError => e
+        fail!(:unknown_signature_algorithm, e)
+      end
+
+      def build_access_token
+        headers = { 'Authorization' => authorization_header }
+        verifier = request.params['code']
+        client.auth_code.get_token(
+          verifier,
+          { :redirect_uri => callback_url, :headers => headers }.merge(token_params.to_hash(:symbolize_keys => true)),
+          deep_symbolize(options.auth_token_params)
+        )
+      end
+
+    private
+
+      def authorization_header
+        header = Base64.encode64("#{options.client_id}:#{options.client_secret}")
+        "Basic #{header}"
       end
 
     end
